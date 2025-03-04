1517722fb212828f1027d93832b6436706f981a9
fisher.huang <317966647@qq.com>
2025-03-04 15:33:40 +0800

    remove dist folder

--- packages/simple-hooks-form/dist/common/FormItem/formList.d.ts
+++
@@ -1,3 +0,0 @@
-import React from "react";
-import { IFormListItemProps } from "../form";
-export declare const FormItemFields: (props: IFormListItemProps) => React.JSX.Element | null;
--- packages/simple-hooks-form/dist/components/NormalForm/index.d.ts
+++
@@ -1,4 +0,0 @@
-import React from "react";
-import { IFormInstance, IFormProps } from "../../common/form";
-declare const NormalForm: React.ForwardRefExoticComponent<IFormProps & React.RefAttributes<IFormInstance>>;
-export default NormalForm;
